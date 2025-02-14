ed5d4943a9e1162f6d706970a7c080c6e633a52f
Vinicius Lopes <vinicius.lopes.230996@gmail.com>
2025-02-14 20:18:14 -0300

    add Prisma service and update AppController to fetch users

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
-@Controller()
+@Controller('/api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
+  @Get('/hello')
+  getHello() {
+    return this.prisma.user.findMany()
   }
 }
