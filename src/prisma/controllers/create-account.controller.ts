1a02aa6657aca900f4c19052316c14cb97e516cc
Vinicius Lopes <vinicius.lopes.230996@gmail.com>
2025-02-14 21:26:26 -0300

    refactor: remove AppController and AppService; add CreateAccountController for user account creation

---
+++ src/prisma/controllers/create-account.controller.ts
@@ -0,0 +1,35 @@
+import {
+  Controller,
+  Post,
+  HttpCode,
+  Body,
+  ConflictException,
+} from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(@Body() body: any) {
+    const { name, email, password } = body
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) throw new ConflictException('Email already exists')
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    })
+  }
+}
