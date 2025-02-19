8eab72cf7e200f6ad2117aef331732e85af97ebe
Vinicius Lopes <vinicius.lopes.230996@gmail.com>
2025-02-19 15:20:24 -0300

    feat: implement authentication and registration use cases for students with cryptography support

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -3,10 +3,24 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 interface StudentProps {
   name: string
+  email: string
+  password: string
 }
 
 export class Student extends Entity<StudentProps> {
-  static create(props: StudentProps, id: UniqueEntityID): Student {
+  get name(): string {
+    return this.props.name
+  }
+
+  get email(): string {
+    return this.props.email
+  }
+
+  get password(): string {
+    return this.props.password
+  }
+
+  static create(props: StudentProps, id?: UniqueEntityID): Student {
     const question = new Student(props, id)
     return question
   }
--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -28,23 +28,5 @@ export class CreateAccountController {
   @UsePipes(new ZodValidationPipe(createAccountBodySchema))
   async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, password } = createAccountBodySchema.parse(body)
-
-    const userWithSameEmail = await this.prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
-
-    if (userWithSameEmail) throw new ConflictException('Email already exists')
-
-    const hashedPassword = await hash(password, 8)
-
-    await this.prisma.user.create({
-      data: {
-        name,
-        email,
-        password: hashedPassword,
-      },
-    })
   }
 }
