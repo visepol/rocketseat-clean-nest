a11ae2ab5e9e30c9b1a455eee4255171327b484f
Vinicius Lopes <vinicius.lopes.230996@gmail.com>
2025-02-18 23:28:45 -0300

    feat: implement create, findBySlug, delete, save, and findManyRecent methods in PrismaQuestionsRepository

--- src/infra/database/prisma/repositories/prisma-question-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-repository.ts
@@ -21,23 +21,52 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
     return PrismaQuestionMapper.toDomain(question)
   }
 
-  create(question: Question): Promise<void> {
-    throw new Error('Method not implemented.')
+  async create(question: Question): Promise<void> {
+    const data = PrismaQuestionMapper.toPersistence(question)
+    await this.prisma.question.create({
+      data,
+    })
   }
 
-  findBySlug(slug: string): Promise<Question | null> {
-    throw new Error('Method not implemented.')
+  async findBySlug(slug: string): Promise<Question | null> {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        slug,
+      },
+    })
+
+    if (!question) return null
+
+    return PrismaQuestionMapper.toDomain(question)
   }
 
-  delete(question: Question): Promise<void> {
-    throw new Error('Method not implemented.')
+  async delete(question: Question): Promise<void> {
+    await this.prisma.question.update({
+      where: {
+        id: question.id,
+      },
+    })
   }
 
-  save(question: Question): Promise<void> {
-    throw new Error('Method not implemented.')
+  async save(question: Question): Promise<void> {
+    const data = PrismaQuestionMapper.toPersistence(question)
+    await this.prisma.question.update({
+      where: {
+        id: data.id?.toString(),
+      },
+      data,
+    })
   }
 
-  findManyRecent(params: PaginationParams): Promise<Question[]> {
-    throw new Error('Method not implemented.')
+  async findManyRecent({ page }: PaginationParams): Promise<Question[]> {
+    const questions = await this.prisma.question.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return questions.map(PrismaQuestionMapper.toDomain)
   }
 }
