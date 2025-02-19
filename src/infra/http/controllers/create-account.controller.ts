32446fb070ffd1803bf487b546c277116e2a6de5
Vinicius Lopes <vinicius.lopes.230996@gmail.com>
2025-02-19 19:05:57 -0300

    feat: refactor CreateAccountController to use RegisterStudentUseCase for account creation

--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -6,10 +6,9 @@ import {
   ConflictException,
   UsePipes,
 } from '@nestjs/common'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
-import { hash } from 'bcryptjs'
 import { z } from 'zod'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
 
 const createAccountBodySchema = z.object({
   name: z.string(),
@@ -21,12 +20,22 @@ type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
 export class CreateAccountController {
-  constructor(private prisma: PrismaService) {}
+  constructor(private registerStudent: RegisterStudentUseCase) {}
 
   @Post()
   @HttpCode(201)
   @UsePipes(new ZodValidationPipe(createAccountBodySchema))
   async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, password } = createAccountBodySchema.parse(body)
+
+    const result = await this.registerStudent.execute({
+      name,
+      email,
+      password,
+    })
+
+    if (result.isLeft()) {
+      throw new ConflictException(result.value)
+    }
   }
 }
