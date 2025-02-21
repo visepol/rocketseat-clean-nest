e098f70b932fcb0602f615a639a9304d21999a6f
Vinicius Lopes <vinicius.lopes.230996@gmail.com>
2025-02-21 04:26:03 -0300

    feat: update Answer and Comment entities to allow nullable updatedAt field; add Prisma mappers for answers and comments

---
+++ src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
@@ -0,0 +1,17 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
+import { Attachment as PrismaAttachment } from '@prisma/client'
+
+export class PrismaQuestionAttachmentMapper {
+  static toDomain(raw: PrismaAttachment): QuestionAttachment {
+    if (!raw.questionId) throw new Error('Invalid comment type.')
+
+    return QuestionAttachment.create(
+      {
+        questionId: new UniqueEntityID(raw.questionId),
+        attachmentId: new UniqueEntityID(raw.id),
+      },
+      new UniqueEntityID(raw.id),
+    )
+  }
+}
---
+++ src/infra/database/prisma/mappers/prisma-question-comment-mapper.ts
@@ -0,0 +1,33 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+import { Comment as PrismaComment, Prisma } from '@prisma/client'
+
+export class PrismaQuestionCommentMapper {
+  static toDomain(raw: PrismaComment): QuestionComment {
+    if (!raw.questionId) throw new Error('Invalid comment type.')
+
+    return QuestionComment.create(
+      {
+        content: raw.content,
+        questionId: new UniqueEntityID(raw.questionId),
+        authorId: new UniqueEntityID(raw.authorId),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityID(raw.id),
+    )
+  }
+
+  static toPersistence(
+    questionComment: QuestionComment,
+  ): Prisma.CommentUncheckedCreateInput {
+    return {
+      id: questionComment.id.toString(),
+      authorId: questionComment.authorId.toString(),
+      questionId: questionComment.questionId.toString(),
+      content: questionComment.content,
+      createdAt: questionComment.createdAt,
+      updatedAt: questionComment.updatedAt,
+    }
+  }
+}
