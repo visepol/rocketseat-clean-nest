3dc97cf05ef5b180e64abbcd686e0867160f4cb8
Vinicius Lopes <vinicius.lopes.230996@gmail.com>
2025-02-22 01:09:25 -0300

    feat: implement EditQuestion use case and controller with E2E tests; refactor repository interfaces to abstract classes

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -9,17 +9,37 @@ import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-re
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
 import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-repository'
+import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
+import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
+import { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository'
+import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
+import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 
 @Module({
   providers: [
     PrismaService,
     { provide: QuestionsRepository, useClass: PrismaQuestionsRepository },
     { provide: StudentsRepository, useClass: PrismaStudentsRepository },
-    PrismaQuestionCommentsRepository,
-    PrismaQuestionAttachmentsRepository,
-    PrismaAnswerAttachmentsRepository,
-    PrismaAnswerCommentsRepository,
-    PrismaAnswersRepository,
+    {
+      provide: QuestionCommentsRepository,
+      useClass: PrismaQuestionCommentsRepository,
+    },
+    {
+      provide: QuestionAttachmentsRepository,
+      useClass: PrismaQuestionAttachmentsRepository,
+    },
+    {
+      provide: AnswerAttachmentsRepository,
+      useClass: PrismaAnswerAttachmentsRepository,
+    },
+    {
+      provide: AnswerCommentsRepository,
+      useClass: PrismaAnswerCommentsRepository,
+    },
+    {
+      provide: AnswersRepository,
+      useClass: PrismaAnswersRepository,
+    },
   ],
   exports: [
     PrismaService,
--- src/infra/http/controllers/edit.question.controller.ts
+++ src/infra/http/controllers/edit.question.controller.ts
@@ -1,5 +1,6 @@
 import { AppModule } from '@/infra/app.module'
-import { DatabaseModule } from '@faker-js/faker/.'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { INestApplication } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { Test } from '@nestjs/testing'
@@ -7,58 +8,57 @@ import request from 'supertest'
 import { QuestionFactory } from 'test/factories/make-question'
 import { StudentFactory } from 'test/factories/make-student'
 
-describe('Fetch recent question (E2E)', () => {
+describe('Edit question (E2E)', () => {
   let app: INestApplication
   let studentFactory: StudentFactory
   let questionFactory: QuestionFactory
+  let prisma: PrismaService
   let jwt: JwtService
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory, QuestionFactory],
     }).compile()
 
     app = moduleRef.createNestApplication()
 
     studentFactory = moduleRef.get(StudentFactory)
+    prisma = moduleRef.get(PrismaService)
     questionFactory = moduleRef.get(QuestionFactory)
+
     jwt = moduleRef.get(JwtService)
 
     await app.init()
   })
 
-  test('[GET] /questions', async () => {
+  test('[PUT] /questions/:id', async () => {
     const user = await studentFactory.makePrismaStudent()
 
-    const acessToken = jwt.sign({ sub: user.id })
+    const acessToken = jwt.sign({ sub: user.id.toString() })
 
-    await Promise.all([
-      questionFactory.makePrismaQuestion({
-        authorId: user.id,
-        title: 'Sample title 1',
-      }),
-      questionFactory.makePrismaQuestion({
-        authorId: user.id,
-        title: 'Sample title 2',
-      }),
-    ])
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    })
+
+    const questionId = question.id.toString()
 
     const response = await request(app.getHttpServer())
-      .get('/questions')
+      .post(`/questions/${questionId}`)
       .set('Authorization', `Bearer ${acessToken}`)
-      .send()
+      .send({
+        title: 'Sample title',
+        content: 'Sample content.',
+      })
 
-    expect(response.statusCode).toBe(200)
-    expect(response.body.questions).toEqual({
-      questions: expect.arrayContaining([
-        expect.objectContaining({
-          title: 'Sample title 1',
-        }),
-        expect.objectContaining({
-          title: 'Sample title 2',
-        }),
-      ]),
+    expect(response.statusCode).toBe(201)
+
+    const questionOnDatabase = await prisma.question.findFirst({
+      where: {
+        title: 'Sample title',
+        content: 'Sample content.',
+      },
     })
+
+    expect(questionOnDatabase).toBeTruthy()
   })
 })
