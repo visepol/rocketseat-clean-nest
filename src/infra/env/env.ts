9813960745aee9b854b275d222bf5c7a0e572606
Vinicius Lopes <vinicius.lopes.230996@gmail.com>
2025-02-23 02:40:12 -0300

    feat: integrate AWS S3 storage with R2Storage implementation; add environment variables and update modules

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -5,6 +5,10 @@ export const envSchema = z.object({
   PORT: z.coerce.number().optional().default(3333),
   JWT_PRIVATE_KEY: z.string(),
   JWT_PUBLIC_KEY: z.string(),
+  CLOUDFLARE_ACCOUNT_ID: z.string(),
+  AWS_ACESS_KEY_ID: z.string(),
+  AWS_SECRET: z.string(),
+  AWS_BUCKET_NAME: z.string(),
 })
 
 export type Env = z.infer<typeof envSchema>
---
+++ src/infra/storage/r2-storage.ts
@@ -0,0 +1,47 @@
+import {
+  Uploader,
+  UploadParams,
+} from '@/domain/forum/application/storage/uploader'
+
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
+import { EnvService } from '../env/env.service'
+import { randomUUID } from 'node:crypto'
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class R2Storage implements Uploader {
+  private client: S3Client
+
+  constructor(private envService: EnvService) {
+    const accountId = envService.get('CLOUDFLARE_ACCOUNT_ID')
+
+    this.client = new S3Client({
+      endpoint: `http://${accountId}:4566`,
+      region: 'auto',
+      credentials: {
+        accessKeyId: envService.get('AWS_ACESS_KEY_ID'),
+        secretAccessKey: envService.get('AWS_SECRET'),
+      },
+    })
+  }
+
+  async upload({
+    fileName,
+    fileType,
+    body,
+  }: UploadParams): Promise<{ url: string }> {
+    const uploadId = randomUUID()
+    const uniqueFileName = `${uploadId}-${fileName}`
+
+    await this.client.send(
+      new PutObjectCommand({
+        Bucket: this.envService.get('AWS_BUCKET_NAME'),
+        Key: uniqueFileName,
+        ContentType: fileType,
+        Body: body,
+      }),
+    )
+
+    return { url: uniqueFileName }
+  }
+}
