1e53b00f020c570930a96177489b88cd25a2658f
Vinicius Lopes <vinicius.lopes.230996@gmail.com>
2025-02-23 01:40:33 -0300

    feat: implement UploadAndCreateAttachment use case with E2E tests; add in-memory repository and fake uploader

---
+++ src/domain/forum/application/use-cases/upload-and-create-attachment.e2e-spec.ts
@@ -0,0 +1,51 @@
+import { InMemoryAttachmentsRepository } from 'test/repositories/in-memory-attachments-repository'
+import { UploadAndCreateAttachmentUseCase } from './upload-and-create-attachment'
+import { FakeUploader } from 'test/storage/fake-uploader'
+import { InvalidAttachmentTypeError } from './errors/invalid-attachment-type'
+
+let inMemoryAttachmentsRepository: InMemoryAttachmentsRepository
+let fakeUploader: FakeUploader
+
+let sut: UploadAndCreateAttachmentUseCase
+
+describe('Upload and create attachment', () => {
+  beforeEach(() => {
+    inMemoryAttachmentsRepository = new InMemoryAttachmentsRepository()
+    fakeUploader = new FakeUploader()
+
+    sut = new UploadAndCreateAttachmentUseCase(
+      inMemoryAttachmentsRepository,
+      fakeUploader,
+    )
+  })
+
+  it('should be able to uplodad and create an attachment', async () => {
+    const result = await sut.execute({
+      fileName: 'file.png',
+      fileType: 'image/png',
+      body: Buffer.from(''),
+    })
+
+    expect(result.isRight()).toBeTruthy()
+    expect(result.value).toEqual({
+      attachment: inMemoryAttachmentsRepository.items[0],
+    })
+    expect(fakeUploader.uploads).toHaveLength(1)
+    expect(fakeUploader.uploads[0]).toEqual(
+      expect.objectContaining({
+        fileName: 'file.png',
+      }),
+    )
+  })
+
+  it('should not be abble to upload an attachment with invalid file type', async () => {
+    const result = await sut.execute({
+      fileName: 'file.mp3',
+      fileType: 'audio/mpeg',
+      body: Buffer.from(''),
+    })
+
+    expect(result.isLeft()).toBeTruthy()
+    expect(result.value).toBeInstanceOf(InvalidAttachmentTypeError)
+  })
+})
